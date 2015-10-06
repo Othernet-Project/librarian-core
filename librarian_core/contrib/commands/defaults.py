666bfcd9bc9716a1d202df5c08d6804cd8463d16
Andrean Franc <andrean.franc@gmail.com>
2015-10-06 17:42:10 +0200

    Add debug-conf command

--- librarian_core/contrib/commands/defaults.py
+++ librarian_core/contrib/commands/defaults.py
@@ -1,8 +1,15 @@
 import os
+import pprint
 
 
 def debug_on(arg, supervisor):
     supervisor.config['app.debug'] = arg
+
+
+def debug_conf(arg, supervisor):
+    print('Configuration file path: %s' % supervisor.config_path)
+    pprint.pprint(supervisor.config, indent=4)
+    raise supervisor.EarlyExit()
 
 
 def install_default_commands(supervisor):
@@ -19,3 +26,9 @@ def install_default_commands(supervisor):
                                       '--debug',
                                       action='store_true',
                                       help='enable debugging')
+    supervisor.exts.commands.register('debug_conf',
+                                      debug_conf,
+                                      '--debug-conf',
+                                      action='store_true',
+                                      help='print out the configuration in '
+                                           'use and exit')
