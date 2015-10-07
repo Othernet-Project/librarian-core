87c113a551369532b36d1ee0769531b7575a48fd
Andrean Franc <andrean.franc@gmail.com>
2015-10-07 22:29:35 +0200

    Add hook to change default root id

--- librarian_core/contrib/system/hooks.py
+++ librarian_core/contrib/system/hooks.py
@@ -1,4 +1,10 @@
+import functools
+
 from .routes import error_403, error_404, error_500, error_503
+
+
+def set_default_route_id(supervisor, route_id):
+    supervisor.config['default_route_id'] = route_id
 
 
 def initialize(supervisor):
@@ -6,3 +12,5 @@ def initialize(supervisor):
     supervisor.app.error(404)(error_404)
     supervisor.app.error(500)(error_500)
     supervisor.app.error(503)(error_503)
+    set_route_id = functools.partial(set_default_route_id, supervisor)
+    supervisor.events.subscribe('set_default_route_id', set_route_id)
