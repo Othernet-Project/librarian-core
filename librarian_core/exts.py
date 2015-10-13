79c5635557d211fb0b39101465db18a6892c1dd7
Andrean Franc <andrean.franc@gmail.com>
2015-10-13 15:29:18 +0200

    Rewrite extension container to support retroactive replays for extension method invocations

--- librarian_core/exts.py
+++ librarian_core/exts.py
@@ -8,6 +8,17 @@ This software is free software licensed under the terms of GPLv3. See COPYING
 file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 """
 
+import functools
+import logging
+
+
+class ExtensionAlreadyExists(Exception):
+    pass
+
+
+class ExtensionNameUnavailable(Exception):
+    pass
+
 
 class Nothing(object):
     """Special object used to indicate that no return value is expected from
@@ -22,22 +33,32 @@ class Placeholder(object):
     :param onfail: Value to be returned or exception to be raised if a method
                    is invoked.
     """
-    def __init__(self, onfail):
-        self.onfail = onfail
+    def __init__(self, name, attrpath, parent, onfail):
+        self.__name = name
+        self.__attrpath = attrpath
+        self.__parent = parent
+        self.__onfail = onfail
 
     def __call__(self, *args, **kwargs):
-        if self.onfail is not Nothing:
+        if self.__onfail is not Nothing:
             # a method was invoked on a non-existent extension with a
             # preconfigured value to be returned or exception raised, if the
             # extension indeed does not exist.
-            if isinstance(self.onfail, Exception):
-                raise self.onfail
-            return self.onfail
-        # default behavior, return a new ``Placeholder`` with the same config
-        return Placeholder(self.onfail)
+            if isinstance(self.__onfail, Exception):
+                raise self.__onfail
+            return self.__onfail
+        # default behavior: store the called method name and params and return
+        # the same ``Placeholder`` object to continue with the charade
+        self.__parent.store_call(self.__name, self.__attrpath, args, kwargs)
+        return self
 
-    def __getattr__(self, name):
-        return Placeholder(self.onfail)
+    def __getattr__(self, attrname):
+        # create a new ``Placeholder`` with the same configuration, just add
+        # the nested attribute name to the exisiting name
+        return Placeholder(self.__name,
+                           '.'.join([self.__attrpath, attrname]),
+                           self.__parent,
+                           self.__onfail)
 
 
 class ExtContainer(object):
@@ -48,15 +69,35 @@ class ExtContainer(object):
     :param onfail: Value to be returned or exception to be raised by a
                    ``Placeholder`` if a method is invoked on a non-existent
                    extension.
-    :param exts:   Used internally for re-creating an ``ExtContainer`` object
-                   It is basically the ``dict`` which contains the registered
-                   extensions.
-    """
-    _members = ('_extensions', '_onfail')
+    :param exts:   Used internally for cloning an ``ExtContainer`` object
+    :param calls:  Used internally for cloning an ``ExtContainer`` object
+    :param ignore: Used internally for cloning an ``ExtContainer`` object
 
-    def __init__(self, onfail=Nothing, exts=None):
+    Usage:
+
+    >>> ec = ExtContainer()
+    >>> ec.missing.nested.method(1, 2, c=3)
+    >>> ec.is_installed('missing')
+    False
+    >>> ec.missing = Missing()  # previously called methods will be replayed
+    >>> ec.is_installed('missing')
+    True
+    >>> ec.this.works.without.errors()
+    >>> ec(onfail=3).this.works.without.errors()
+    3
+    >>> err = Exception('test')
+    >>> ec(onfail=err).this.works.without.errors()
+    Traceback (most recent call last):
+        ...
+    Exception: test
+    """
+    _members = ('_extensions', '_onfail', '_calls', '_ignore')
+
+    def __init__(self, onfail=Nothing, exts=None, calls=None, ignore=None):
         self._onfail = onfail
         self._extensions = exts or dict()
+        self._calls = calls or dict()
+        self._ignore = ignore or []
 
     def __get_extension(self, name):
         # ``object.__getattribute__`` must be used to avoid infinite loops by
@@ -70,7 +111,44 @@ class ExtContainer(object):
             return exts[name]
         except KeyError:
             onfail = object.__getattribute__(self, '_onfail')
-            return Placeholder(onfail)
+            return Placeholder(name=name,
+                               attrpath='',
+                               parent=self,
+                               onfail=onfail)
+
+    def __install_extension(self, name, extension):
+        # protect against overwriting existing extensions
+        if name in self._extensions:
+            raise ExtensionAlreadyExists(name)
+        # protect against using an extension name that is a valid attribute on
+        # the parent container itself
+        if name in dir(self):
+            raise ExtensionNameUnavailable(name)
+
+        self._extensions[name] = extension
+        self.__replay_calls(name, extension)
+
+    def __invoke(self, extension, attrpath, args, kwargs):
+        attrs = [attr for attr in attrpath.split('.') if attr]
+        try:
+            method = functools.reduce(getattr, attrs, extension)
+        except AttributeError:
+            logging.error("Recorded calls on {0} cannot be replayed "
+                          "retroactively.".format(attrpath))
+        else:
+            method(*args, **kwargs)
+
+    def __replay_calls(self, name, extension):
+        calls = self._calls.pop(name, None)
+        if not calls:  # there were no registered calls, nothing to be done
+            return
+
+        # retroactively invoke all the methods on the actual extension
+        for (attrpath, args, kwargs) in calls:
+            self.__invoke(extension, attrpath, args, kwargs)
+
+    def __getattr__(self, name):
+        return self.__get_extension(name)
 
     def __setattr__(self, name, extension):
         # All assignments are delegated to the internal dict structure, unless
@@ -79,21 +157,27 @@ class ExtContainer(object):
         if name in self._members:
             super(ExtContainer, self).__setattr__(name, extension)
         else:
-            self._extensions[name] = extension
+            self.__install_extension(name, extension)
 
-    def __getattr__(self, name):
-        return self.__get_extension(name)
-
-    def __getitem__(self, name):
-        return self.__get_extension(name)
-
+    __getitem__ = __getattr__
     __setitem__ = __setattr__
 
     def __call__(self, onfail):
         # re-create an ``ExtContainer`` with the same existing extensions, but
         # if methods are invoked on a non-existent extension, ``onfail`` will
         # be used as a return value, instead of another ``Placeholder`` object.
-        return ExtContainer(onfail=onfail, exts=self._extensions)
+        return ExtContainer(onfail=onfail,
+                            exts=self._extensions,
+                            calls=self._calls,
+                            ignore=self._ignore)
+
+    def store_call(self, name, attrpath, args, kwargs):
+        """Called by ``Placeholder`` objects to record calls to extension
+        methods which will be replayed later when the extension is installed.
+        """
+        if name not in self._ignore:
+            self._calls.setdefault(name, [])
+            self._calls[name].append((attrpath, args, kwargs))
 
     def is_installed(self, name):
         """Check whether extension known by ``name`` is installed or not.
@@ -101,3 +185,21 @@ class ExtContainer(object):
         :param name:  name of the extension that was supposedly installed
         """
         return name in self._extensions
+
+    def flush(self, *extensions):
+        """Delete the recorded calls for the specified extensions, or all calls
+        if no specific names are provided.
+
+        :param extensions:  names of extensions
+        """
+        names = extensions or self._calls.keys()
+        for key in names:
+            self._calls.pop(key, None)
+
+    def ignore_calls_from(self, *extensions):
+        """Add names of specified extensions to ignore list so no calls will be
+        recorded for them while they are not installed.
+
+        :param extensions:  names of extensions
+        """
+        self._ignore.extend(extensions)
--- tests/test_exts.py
+++ tests/test_exts.py
@@ -4,35 +4,236 @@ import pytest
 from librarian_core import exts as mod
 
 
-def test_placeholder():
-    placeholder = mod.Placeholder(mod.Nothing)
-    assert isinstance(placeholder(1, 2, a='b'), mod.Placeholder)
-    assert isinstance(placeholder.test_func(1, 2, a='b'), mod.Placeholder)
+class TestPlaceholder(object):
+
+    def test___getattr__(self):
+        container = mock.Mock()
+        placeholder = mod.Placeholder('extname', 'nested', container, 1)
+        cloned = placeholder.some_attr
+        assert isinstance(cloned, mod.Placeholder)
+        assert cloned._Placeholder__name == 'extname'
+        assert cloned._Placeholder__attrpath == 'nested.some_attr'
+        assert cloned._Placeholder__parent is container
+        assert cloned._Placeholder__onfail == 1
+
+    def test___call___onfail_returns(self):
+        placeholder = mod.Placeholder('extname', '', mock.Mock(), 1)
+        assert placeholder() == 1
+
+    def test___call___onfail_raises(self):
+        class CustomError(Exception):
+            pass
+
+        exc = CustomError()
+        placeholder = mod.Placeholder('extname', '', mock.Mock(), exc)
+        with pytest.raises(CustomError):
+            placeholder()
+
+    def test___call___stores_params(self):
+        container = mock.Mock()
+        placeholder = mod.Placeholder('extname', '', container, mod.Nothing)
+        ret = placeholder(1, 2, a='b')
+        assert ret is placeholder
+        container.store_call.assert_called_once_with('extname',
+                                                     '',
+                                                     (1, 2),
+                                                     dict(a='b'))
 
 
-def test_add_extension():
-    exts = mod.ExtContainer()
-    assert exts._extensions == {}
-    exts.first = 1
-    exts['second'] = 2
-    assert exts._extensions == {'first': 1, 'second': 2}
+class TestExtContainer(object):
+
+    def test___get_extension_found(self):
+        extension = mock.Mock()
+        ec = mod.ExtContainer()
+        ec._extensions['test'] = extension
+        assert ec._ExtContainer__get_extension('test') is extension
+
+    def test___get_extension_not_found(self):
+        ec = mod.ExtContainer()
+        ph = ec.missing
+        assert isinstance(ph, mod.Placeholder)
+        assert ph._Placeholder__name == 'missing'
+        assert ph._Placeholder__attrpath == ''
+        assert ph._Placeholder__parent is ec
+        assert ph._Placeholder__onfail == mod.Nothing
+
+    def test___install_extension_already_exists(self):
+        ec = mod.ExtContainer()
+        ec._extensions['test'] = 1
+        with pytest.raises(mod.ExtensionAlreadyExists):
+            ec._ExtContainer__install_extension('test', 2)
+
+    def test___install_extension_name_unavailable(self):
+        ec = mod.ExtContainer()
+        with pytest.raises(mod.ExtensionNameUnavailable):
+            ec._ExtContainer__install_extension('is_installed', 2)
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__replay_calls')
+    def test___install_extension_success(self, __replay_calls):
+        ext = mock.Mock()
+        ec = mod.ExtContainer()
+        ec._ExtContainer__install_extension('test', ext)
+        assert ec._extensions['test'] is ext
+        __replay_calls.assert_called_once_with('test', ext)
+
+    def test___invoke_no_attrs(self):
+        ext = mock.Mock()
+        ec = mod.ExtContainer()
+        ec._ExtContainer__invoke(ext, '', (1, 2), dict(a=3))
+        ext.assert_called_once_with(1, 2, a=3)
+
+    def test___invoke_with_attrs(self):
+        ext = mock.Mock()
+        ext.nested.method = mock.Mock()
+        ec = mod.ExtContainer()
+        ec._ExtContainer__invoke(ext, 'nested.method', (1, 2), dict(a=3))
+        ext.nested.method.assert_called_once_with(1, 2, a=3)
+
+    @mock.patch.object(mod.logging, 'error')
+    def test___invoke_attr_not_found(self, error):
+        ext = mock.Mock(spec=['valid_method'])
+        ec = mod.ExtContainer()
+
+        with pytest.raises(AttributeError):
+            ext.missing_method
+
+        ec._ExtContainer__invoke(ext, 'missing_method', (1, 2), dict(a=3))
+        assert error.called
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__invoke')
+    def test___replay_calls_found(self, __invoke):
+        ec = mod.ExtContainer()
+        call1 = ('method_a', (1, 2), dict(a=3))
+        call2 = ('nested.method_b', (3, 4), dict(b=4))
+        ec._calls['extname'] = [call1, call2]
+        ext = mock.Mock()
+        ec._ExtContainer__replay_calls('extname', ext)
+        __invoke.assert_has_calls([mock.call(ext, *call1),
+                                   mock.call(ext, *call2)])
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__invoke')
+    def test___replay_calls_cleans_up_properly(self, __invoke):
+        ec = mod.ExtContainer()
+        call1 = ('method_a', (1, 2), dict(a=3))
+        call2 = ('nested.method_b', (3, 4), dict(b=4))
+        ec._calls['extname'] = [call1]
+        ec._calls['another'] = [call2]
+        ext = mock.Mock()
+        ec._ExtContainer__replay_calls('extname', ext)
+        assert 'extname' not in ec._calls
+        assert 'another' in ec._calls
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__invoke')
+    def test___replay_calls_not_found(self, __invoke):
+        ec = mod.ExtContainer()
+        ext = mock.Mock()
+        ec._ExtContainer__replay_calls('missing', ext)
+        assert not __invoke.called
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__install_extension')
+    def test___setattr___member(self, __install_extension):
+        ec = mod.ExtContainer()
+        assert ec._onfail == mod.Nothing
+        ec._onfail = 'test'
+        assert ec._onfail == 'test'
+        assert not __install_extension.called
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__install_extension')
+    def test___setattr___nonmember(self, __install_extension):
+        ec = mod.ExtContainer()
+        ext = mock.Mock()
+        ec.test = ext
+        assert 'test' not in dir(ec)
+        __install_extension.assert_called_once_with('test', ext)
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__install_extension')
+    def test___setitem__(self, __install_extension):
+        ec = mod.ExtContainer()
+        ext = mock.Mock()
+        ec['test'] = ext
+        assert 'test' not in dir(ec)
+        __install_extension.assert_called_once_with('test', ext)
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__get_extension')
+    def test___getattr__(self, __get_extension):
+        ec = mod.ExtContainer()
+        assert ec.extname == __get_extension.return_value
+        __get_extension.assert_called_once_with('extname')
+
+    @mock.patch.object(mod.ExtContainer, '_ExtContainer__get_extension')
+    def test___getitem__(self, __get_extension):
+        ec = mod.ExtContainer()
+        assert ec['extname'] == __get_extension.return_value
+        __get_extension.assert_called_once_with('extname')
+
+    def test___call__(self):
+        ec = mod.ExtContainer()
+        ec._extensions['test'] = 1
+        ec._calls['test'] = [(1, 2, 3)]
+        second = ec(onfail='test')
+        assert second._onfail == 'test'
+        assert second._extensions == ec._extensions
+        assert second._calls == ec._calls
+        assert second._ignore == ec._ignore
+
+    def test_store_call(self):
+        ec = mod.ExtContainer()
+        ec.store_call('extname', 'method', (1, 2), dict(a=3))
+        assert len(ec._calls) == 1
+        assert ec._calls == {'extname': [('method', (1, 2), dict(a=3))]}
+
+    def test_store_call_ignore(self):
+        ec = mod.ExtContainer()
+        ec._ignore = ['extname']
+        ec.store_call('extname', 'method', (1, 2), dict(a=3))
+        assert len(ec._calls) == 0
+
+    def test_is_installed(self):
+        ec = mod.ExtContainer()
+        ec['valid'] = 1
+        assert ec.is_installed('valid')
+        assert not ec.is_installed('missing')
+
+    def test_flush_all(self):
+        ec = mod.ExtContainer()
+        ec._calls['ext1'] = [1, 2]
+        ec._calls['ext2'] = [4, 5]
+        ec.flush()
+        assert len(ec._calls) == 0
+
+    def test_flush_specific(self):
+        ec = mod.ExtContainer()
+        ec._calls['ext1'] = [1, 2]
+        ec._calls['ext2'] = [4, 5]
+        ec._calls['ext3'] = [7, 8]
+        ec.flush('ext1', 'ext3')
+        assert len(ec._calls) == 1
+        assert 'ext1' not in ec._calls
+        assert 'ext3' not in ec._calls
+        assert ec._calls['ext2'] == [4, 5]
+
+    def test_ignore_calls_from(self):
+        ec = mod.ExtContainer()
+        assert ec._ignore == []
+        ec.ignore_calls_from('a', 'b')
+        assert ec._ignore == ['a', 'b']
 
 
-def test_access_extension():
+def test_integration_access_extension():
     test_ext = mock.Mock()
-    exts = mod.ExtContainer()
-    exts.important = test_ext
-    exts.important(1, 2)
+    ec = mod.ExtContainer()
+    ec.important = test_ext
+    ec.important(1, 2)
     test_ext.assert_called_once_with(1, 2)
     try:
-        exts.not_installeld('cache')
+        ec.not_installed('cache')
     except Exception:
         pytest.fail("Should not care whether it exists or not.")
 
 
-def test_onfail_of_missing_ext():
-    exts = mod.ExtContainer()
-    result = exts(onfail='return this').missing_ext.method(a=1)
+def test_integration_onfail_of_missing_ext():
+    ec = mod.ExtContainer()
+    result = ec(onfail='return this').missing_ext.method(a=1)
     assert result == 'return this'
 
     class CustomError(Exception):
@@ -40,11 +241,18 @@ def test_onfail_of_missing_ext():
 
     exc = CustomError()
     with pytest.raises(CustomError):
-        exts(onfail=exc).missing()
+        ec(onfail=exc).missing()
 
 
-def test_is_installed():
-    exts = mod.ExtContainer()
-    exts['valid'] = 1
-    assert exts.is_installed('valid')
-    assert not exts.is_installed('missing')
+def test_integration_replay_calls():
+    ext = mock.Mock()
+    ec = mod.ExtContainer()
+    ec.not_yet_here('initializing', kw=42)
+    ec.not_yet_here.method1(1, 2, a=4)
+    ec.not_yet_here.nested.method2(5, 7, g=9)
+
+    ec.not_yet_here = ext
+
+    ext.assert_called_once_with('initializing', kw=42)
+    ext.method1.assert_called_once_with(1, 2, a=4)
+    ext.nested.method2.assert_called_once_with(5, 7, g=9)
