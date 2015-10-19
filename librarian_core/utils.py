59958ccdee77a9913f27f94385b8e47d3bda871e
Andrean Franc <andrean.franc@gmail.com>
2015-10-19 11:57:55 +0200

    Add py2/3 compatible utility function for checking if passed argument is string

--- librarian_core/utils.py
+++ librarian_core/utils.py
@@ -1,1 +1,8 @@
 from .contrib.databases.utils import utcnow  # NOQA
+
+
+def is_string(obj):
+    try:
+        return isinstance(obj, basestring)
+    except NameError:
+        return isinstance(obj, str)
