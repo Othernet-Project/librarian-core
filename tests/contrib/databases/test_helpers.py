5db138500ca085b332fe120a599fc6bf00bc0af6
Andrean Franc <andrean.franc@gmail.com>
2015-10-20 12:12:34 +0200

    Fix contrib.databases.helpers unittest

--- tests/contrib/databases/test_helpers.py
+++ tests/contrib/databases/test_helpers.py
@@ -1,12 +1,9 @@
 import mock
 
-from librarian_core.contrib.databases import utils as mod
+from librarian_core.contrib.databases import helpers as mod
 
 
-MOD = mod.__name__
-
-
-@mock.patch(MOD + '.Database.connect')
+@mock.patch.object(mod.Database, 'connect')
 def test_get_databases_connects(db_connect):
     """ When database name is passed as string, connection is made """
     mod.get_databases({'foo': {'package_name': 'foopkg', 'path': 'foo.db'}})
