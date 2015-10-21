9a4ffa53948b05dc5c7617a49648f216ea62fede
Andrean Franc <andrean.franc@gmail.com>
2015-10-21 19:10:34 +0200

    Move configurable version information query utils into core.contrib.system

--- librarian_core/contrib/system/hooks.py
+++ librarian_core/contrib/system/hooks.py
@@ -1,3 +1,4 @@
+from .commands import display_version
 from .logs import configure
 from .routes import error_403, error_404, error_500, error_503
 
@@ -8,3 +9,8 @@ def initialize(supervisor):
     supervisor.app.error(404)(error_404)
     supervisor.app.error(500)(error_500)
     supervisor.app.error(503)(error_503)
+    supervisor.exts.commands.register('version',
+                                      display_version,
+                                      '--version',
+                                      action='store_true',
+                                      help='print out version number and exit')
