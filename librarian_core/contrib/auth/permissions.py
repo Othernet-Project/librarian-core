579e3fd29a21ff9311bcbdf130133aa509ea1a3d
Andrean Franc <andrean.franc@gmail.com>
2015-10-22 14:30:40 +0200

    Initial acl permission implementation

--- librarian_core/contrib/auth/permissions.py
+++ librarian_core/contrib/auth/permissions.py
@@ -29,3 +29,63 @@ class BaseDynamicPermission(BasePermission):
                             where='name = :name AND identifier = :identifier')
         data = json.dumps(self.data, cls=DateTimeEncoder)
         self.db.query(q, name=self.name, identifier=self.identifier, data=data)
+
+
+class ACLPermission(BaseDynamicPermission):
+    NO_PERMISSION = 0
+    READ = 4
+    WRITE = 2
+    EXECUTE = 1
+    ALIASES = {
+        'r': READ,
+        'w': WRITE,
+        'x': EXECUTE,
+        READ: READ,
+        WRITE: WRITE,
+        EXECUTE: EXECUTE
+    }
+
+    def grant(self, path, permission):
+        existing = self.data.get(path, self.NO_PERMISSION)
+        if existing & permission:
+            # permission already granted, so just keep as it is
+            new_permission = existing
+        else:
+            # add to existing permission
+            new_permission = existing + permission
+
+        self.data[path] = new_permission
+        self.save()
+
+    def revoke(self, path, permission):
+        existing = self.data.get(path, self.NO_PERMISSION)
+        if existing & permission:
+            # contains permission so it needs to be revoked
+            new_permission = existing - permission
+        else:
+            # didn't even have that permission, so just retain the existing one
+            new_permission = existing
+
+        if new_permission == self.NO_PERMISSION:
+            # when having no permission, we can freely just remove the whole
+            # path as not having a path at all also means having no permissions
+            # whatsoever
+            self.data.pop(path, None)
+        else:
+            self.data[path] = new_permission
+
+        self.save()
+
+    def clear(self):
+        self.data[self.DATA_KEY] = {}
+        self.save()
+
+    def is_granted(self, path, permission='r'):
+        try:
+            permission = self.ALIASES[permission]
+        except KeyError:
+            msg = "Invalid permission specified: {0}".format(permission)
+            raise ValueError(msg)
+        else:
+            existing = self.data.get(path, self.NO_PERMISSION)
+            return existing & permission
