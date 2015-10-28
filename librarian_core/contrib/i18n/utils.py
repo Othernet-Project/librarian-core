3bad14a7238d3e10072917ec790adc52e520feba
Andrean Franc <andrean.franc@gmail.com>
2015-10-28 15:18:37 +0100

    Add helper to set current locale on request and response objects

--- librarian_core/contrib/i18n/utils.py
+++ librarian_core/contrib/i18n/utils.py
@@ -1,10 +1,15 @@
-from bottle import request
+from bottle import request, response
 
 
 def set_default_locale(code):
     for plugin in request.app.plugins:
         if getattr(plugin, 'name', '') == 'i18n':
             plugin.default_locale = code
+
+
+def set_current_locale(code):
+    request.locale = code
+    response.set_cookie('locale', code, path='/')
 
 
 def get_enabled_locales():
