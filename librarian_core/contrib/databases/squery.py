ab371e21bd58f15db269f9eb40df5efd424b4b8e
Andrean Franc <andrean.franc@gmail.com>
2015-11-24 10:12:04 +0100

    Make database class use connection pool instead of our connection objects

--- librarian_core/contrib/databases/squery.py
+++ librarian_core/contrib/databases/squery.py
@@ -10,65 +10,22 @@ file that comes with the source code, or http://www.gnu.org/licenses/gpl.txt.
 
 from __future__ import print_function
 
-import contextlib
-import logging
-import os
+import functools
 import re
 
-import fdb as dblib
+import psycopg2
 
 from bottle_utils.common import basestring
-from bottle_utils.lazy import CachingLazy
 from sqlize import (From, Where, Group, Order, Limit, Select, Update, Delete,
                     Insert, Replace, sqlin, sqlarray)
 
+from .pool import PostgresConnectionPool
 
+
+AUTOCOMMIT = psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT
 SLASH = re.compile(r'\\')
 MAX_VARIABLE_NUMBER = 999
-
-
-class Connection(object):
-    """ Wrapper for dblib.Connection object """
-    def __init__(self, host, port, database, user, password):
-        self.host = host
-        self.port = port
-        self.database = database
-        self.user = user
-        self.password = password
-        self.connect()
-
-    def connect(self):
-        connector = dblib.connect
-        if not os.path.exists(self.database):
-            connector = dblib.create_database
-
-        self._conn = connector(host=self.host,
-                               port=self.port,
-                               database=self.database,
-                               user=self.user,
-                               password=self.password)
-        logging.debug('Connected to database {}'.format(self.dsn))
-
-    def close(self):
-        self._conn.commit()
-        self._conn.close()
-
-    @property
-    def dsn(self):
-        return '{}:{}/{}'.format(self.host, self.port, self.database)
-
-    def __getattr__(self, attr):
-        conn = object.__getattribute__(self, '_conn')
-        return getattr(conn, attr)
-
-    def __setattr__(self, attr, value):
-        if not hasattr(self, attr) or attr == '_conn':
-            object.__setattr__(self, attr, value)
-        else:
-            setattr(self._conn, attr, value)
-
-    def __repr__(self):
-        return "<Connection '%s'>" % self.dsn
+DEFAULT_MAX_POOL_SIZE = 5
 
 
 class Database(object):
@@ -88,113 +45,108 @@ class Database(object):
     Replace = Replace
     MAX_VARIABLE_NUMBER = MAX_VARIABLE_NUMBER
 
-    def __init__(self, conn, debug=False):
-        self.conn = conn
+    def __init__(self, pool, connection_params, debug=False):
+        self.pool = pool
+        self.connection_params = connection_params
         self.debug = debug
-        self._cursor = None
 
-    def _convert_query(self, qry):
-        """ Ensure any SQLExpression instances are serialized
+    def serialize_query(func):
+        """ Ensure any SQLExpression instances are serialized"""
+        @functools.wraps(func)
+        def wrapper(self, query, *args, **kwargs):
+            if hasattr(query, 'serialize'):
+                query = query.serialize()
 
-        :param qry:     raw SQL string or SQLExpression instance
-        :returns:       raw SQL string
-        """
-        if hasattr(qry, 'serialize'):
-            qry = qry.serialize()
-        assert isinstance(qry, basestring), 'Expected qry to be string'
-        if self.debug:
-            print('SQL:', qry)
-        return qry
+            assert isinstance(query, basestring), 'Expected query to be string'
+            if self.debug:
+                print('SQL:', query)
 
-    def query(self, qry, *params, **kwparams):
-        """ Perform a query
+            return func(self, query, *args, **kwargs)
+        return wrapper
 
-        Any positional arguments are converted to a list of arguments for the
-        query, and are used to populate any '?' placeholders. The keyword
-        arguments are converted to a mapping which provides values to ':name'
-        placeholders. These do not apply to SQLExpression instances.
+    @serialize_query
+    def execute(self, query, *args, **kwargs):
+        return self.pool.execute(query, *args, **kwargs)
 
-        :param qry:     raw SQL or SQLExpression instance
-        :returns:       cursor object
-        """
-        qry = self._convert_query(qry)
-        self.cursor.execute(qry, params or kwparams)
-        return self.cursor
-
-    def execute(self, qry, *args, **kwargs):
-        qry = self._convert_query(qry)
-        self.cursor.execute(qry, *args, **kwargs)
-
-    def executemany(self, qry, *args, **kwargs):
-        qry = self._convert_query(qry)
-        self.cursor.executemany(qry, *args, **kwargs)
+    @serialize_query
+    def executemany(self, query, *args, **kwargs):
+        return self.pool.executemany(query, *args, **kwargs)
 
     def executescript(self, sql):
-        self.cursor.executescript(sql)
+        return self.pool.execute(sql, isolation_level=AUTOCOMMIT)
 
-    def commit(self):
-        self.conn.commit()
+    @serialize_query
+    def fetchone(self, query, *args, **kwargs):
+        return self.pool.fetchone(query, *args, **kwargs)
 
-    def rollback(self):
-        self.conn.rollback()
-        self.conn.commit()
+    @serialize_query
+    def fetchall(self, query, *args, **kwargs):
+        return self.pool.fetchall(query, *args, **kwargs)
 
-    def refresh_table_stats(self):
-        self.execute('ANALYZE sqlite_master;')
+    @serialize_query
+    def fetchiter(self, query, *args, **kwargs):
+        return self.pool.fetchiter(query, *args, **kwargs)
 
-    def acquire_lock(self):
-        self.execute('BEGIN EXCLUSIVE;')
+    def transaction(self, *args, **kwargs):
+        return self.pool.cursor(*args, **kwargs)
 
     def close(self):
-        self.conn.close()
-        # the cached cursor object must be reset, otherwise after reconnecting
-        # it would still try to use it, and would run into the closed db issue
-        self._cursor = None
-
-    def reconnect(self):
-        self.conn.connect()
+        self.pool.closeall()
 
     @property
-    def connection(self):
-        return self.conn
+    def name(self):
+        return self.kwargs.get('dbname')
 
-    @property
-    def cursor(self):
-        if self._cursor is None:
-            self._cursor = self.conn.cursor()
-        return self._cursor
-
-    @property
-    def results(self):
-        return self.cursor.fetchall()
-
-    @property
-    def result(self):
-        return self.cursor.fetchone()
-
-    @contextlib.contextmanager
-    def transaction(self, silent=False):
-        self.execute('BEGIN;')
-        try:
-            yield self.cursor
-            self.commit()
-        except Exception:
-            self.rollback()
-            if silent:
-                return
-            raise
+    @staticmethod
+    def command(host, port, dbname, user, password, maxsize, sql):
+        pool = PostgresConnectionPool(host=host,
+                                      port=port,
+                                      dbname='postgres',
+                                      user=user,
+                                      password=password,
+                                      maxsize=maxsize)
+        pool.execute(sql, isolation_level=AUTOCOMMIT)
 
     @classmethod
-    def connect(cls, host, port, path, user, password):
-        return Connection(host, port, path, user, password)
+    def create(cls, host, port, dbname, user, password, maxsize):
+        sql = 'CREATE DATABASE {};'.format(dbname)
+        cls.command(host, port, dbname, user, password, maxsize, sql)
 
-    def __repr__(self):
-        return "<Database connection='%s'>" % self.conn.dsn
+    @classmethod
+    def drop(cls, host, port, dbname, user, password, maxsize):
+        sql = 'DROP DATABASE {};'.format(dbname)
+        cls.command(host, port, dbname, user, password, maxsize, sql)
+
+    def recreate(self):
+        self.close()
+        self.drop(**self.connection_params)
+        self.create(**self.connection_params)
+
+    @classmethod
+    def connect(cls, host, port, database, user, password,
+                maxsize=DEFAULT_MAX_POOL_SIZE, debug=False):
+        kwargs = dict(host=host,
+                      port=port,
+                      dbname=database,
+                      user=user,
+                      password=password,
+                      maxsize=maxsize)
+        pool = PostgresConnectionPool(**kwargs)
+        try:
+            conn = pool.create_connection()  # testing connection
+        except psycopg2.OperationalError as exc:
+            if 'does not exist' in str(exc):
+                cls.create(**kwargs)
+            else:
+                raise
+        else:
+            conn.close()  # close this connection as pool manages them later
+
+        return cls(pool, kwargs, debug=debug)
 
 
 class DatabaseContainer(dict):
-    def __init__(self, connections, debug=False):
-        super(DatabaseContainer, self).__init__(
-            {n: CachingLazy(Database, c, debug=debug)
-             for n, c in connections.items()})
+
+    def __init__(self, databases):
+        super(DatabaseContainer, self).__init__(databases)
         self.__dict__ = self
